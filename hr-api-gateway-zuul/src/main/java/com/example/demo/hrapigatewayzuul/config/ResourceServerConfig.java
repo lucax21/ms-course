a1cbc115ccb9d179e749c52119ef2628e0d395c4
jj <lucax.oliveira96@gmail.com>
2022-10-20 10:56:38 -0300

    Security configs to config server

--- hr-api-gateway-zuul/src/main/java/com/example/demo/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/example/demo/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.hrapigatewayzuul.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -19,7 +20,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	
 	private static final String[] OPERATOR = {"/hr-worker/**"};
 	
-	private static final String[] ADMIN = {"hr-payroll/**", "/hr-user/**"};
+	private static final String[] ADMIN = {"hr-payroll/**", "/hr-user/**", "/actuator/**", "hr-worker/actuator/**" ,"/hr-oauth/actuator/**"};
 			
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -28,7 +29,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		
+		http.authorizeRequests()
+		.antMatchers(PUBLIC).permitAll()
+		.antMatchers(HttpMethod.GET, OPERATOR).hasAnyRole("OPERATOR", "ADMIN")
+		.antMatchers(ADMIN).hasAnyRole("ADMIN")
+		.anyRequest().authenticated();
 	}
 	
 }
